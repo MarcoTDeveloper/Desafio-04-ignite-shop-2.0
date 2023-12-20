6382d8b00eba0b1ab35ccd599193b1bd9a4d7ba2
Marco Túlio Rodrigues da Silveira <marcosdevprofessional@gmail.com>
2023-12-20 13:45:42 -0300

    v23.1.0

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,71 +1,84 @@
-import { HomeContainer, Product } from "@/styles/pages/home";
+import { HomeContainer, Product, SliderContainer } from "@/styles/pages/home";
 import Image from "next/image";
 
 import cmEsp from '@/assets/camiseta-especial.png'
 import { Handbag } from "phosphor-react";
+import useEmblaCarousel from "embla-carousel-react";
 
 export default function Home() {
+  const [ emblaRef ] = useEmblaCarousel({
+    align: "start",
+    skipSnaps: false,
+    dragFree: true,
+  })
+
   return (
-    <HomeContainer>
-      <Product>
-        <Image src={cmEsp} width={520} height={480} alt="" />
+    <div style={{ overflow: "hidden", width: "100%"}}>
+      <HomeContainer>
+        <div className="embla" ref={emblaRef}>
+          <SliderContainer className="embla__container container">
+          <Product className="embla__slide">
+            <Image src={cmEsp} width={520} height={480} alt="" />
 
-        <footer>
-          <div>
-            <strong>Camiseta Beyond the Limits</strong>
-            <span>R$ 99,90</span>
-          </div>
+            <footer>
+              <div>
+                <strong>Camiseta Beyond the Limits</strong>
+                <span>R$ 99,90</span>
+              </div>
 
-          <button>
-            <Handbag size={32} weight='bold' />
-          </button>
-        </footer>
-      </Product>
+              <button>
+                <Handbag size={32} weight='bold' />
+              </button>
+            </footer>
+          </Product>
 
-      <Product>
-        <Image src={cmEsp} width={520} height={480} alt="" />
+          <Product className="embla__slide" >
+            <Image src={cmEsp} width={520} height={480} alt="" />
 
-        <footer>
-          <div>
-            <strong>Camiseta Beyond the Limits</strong>
-            <span>R$ 99,90</span>
-          </div>
+            <footer>
+              <div>
+                <strong>Camiseta Beyond the Limits</strong>
+                <span>R$ 99,90</span>
+              </div>
 
-          <button>
-            <Handbag size={32} weight='bold' />
-          </button>
-        </footer>
-      </Product>
+              <button>
+                <Handbag size={32} weight='bold' />
+              </button>
+            </footer>
+          </Product>
 
-      <Product>
-        <Image src={cmEsp} width={520} height={480} alt="" />
+          <Product className="embla__slide" >
+            <Image src={cmEsp} width={520} height={480} alt="" />
 
-        <footer>
-          <div>
-            <strong>Camiseta Beyond the Limits</strong>
-            <span>R$ 99,90</span>
-          </div>
+            <footer>
+              <div>
+                <strong>Camiseta Beyond the Limits</strong>
+                <span>R$ 99,90</span>
+              </div>
 
-          <button>
-            <Handbag size={32} weight='bold' />
-          </button>
-        </footer>
-      </Product>
+              <button>
+                <Handbag size={32} weight='bold' />
+              </button>
+            </footer>
+          </Product>
 
-      <Product>
-        <Image src={cmEsp} width={520} height={480} alt="" />
+          <Product className="embla__slide" >
+            <Image src={cmEsp} width={520} height={480} alt="" />
 
-        <footer>
-          <div>
-            <strong>Camiseta Beyond the Limits</strong>
-            <span>R$ 99,90</span>
-          </div>
+            <footer>
+              <div>
+                <strong>Camiseta Beyond the Limits</strong>
+                <span>R$ 99,90</span>
+              </div>
 
-          <button>
-            <Handbag size={32} weight='bold' />
-          </button>
-        </footer>
-      </Product>
+              <button>
+                <Handbag size={32} weight='bold' />
+              </button>
+            </footer>
+          </Product>
+          </SliderContainer>
+        </div>
     </HomeContainer>
+    </div>
   )
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -9,12 +9,24 @@ export const HomeContainer = styled('main', {
   minHeight: 656,
 })
 
+export const SliderContainer = styled('div', {
+  display: 'flex',
+  gap: '3rem',
+  margin: '0 auto', 
+
+  ".embla__slide": {
+    minWidth: '43.5rem',
+  },
+})
+
 export const Product = styled('div', {
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
   borderRadius: 8,
-  minWidth: 696,
   position: 'relative',
   overflow: 'hidden',
+  width: '100%',
+  minHeight: 656,
+
 
   display: 'flex',
   alignItems: 'center',
