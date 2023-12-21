b1199b9c6d14d3313f28e04293fb75e8727c3a8f
Marco Túlio Rodrigues da Silveira <marcosdevprofessional@gmail.com>
2023-12-21 17:34:02 -0300

    v23.1.0

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,29 +9,41 @@ import { HeaderButton, Container, Header } from '@/styles/pages/app'
 
 import * as Dialog from '@radix-ui/react-dialog';
 import { ShoppingCart } from '@/components/ShoppingCart'
+import { useRouter } from 'next/router'
 
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  console.log(router);
+
 
   return (
     <Container>
-      <Header>
-        <Image src={logoImg} alt='' />
 
-        <Dialog.Root>
-          <Dialog.Trigger asChild>
-            <HeaderButton><Handbag size={24} weight='bold' /></HeaderButton>
-          </Dialog.Trigger>
+      {router.pathname !== "/success" ? (
+        <Header>
+          <Image src={logoImg} alt='' />
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <HeaderButton><Handbag size={24} weight='bold' /></HeaderButton>
+            </Dialog.Trigger>
+            <ShoppingCart />
+          </Dialog.Root>
 
-          <ShoppingCart />
-
-        </Dialog.Root>
-
-      </Header>
+        </Header>
+      ) :
+        <Header
+          style={{ justifyContent: 'center' }}
+        >
+          <Image src={logoImg} alt='' />
+        </Header>
+      }
 
       <Component {...pageProps} />
     </Container>
   )
 }
+
